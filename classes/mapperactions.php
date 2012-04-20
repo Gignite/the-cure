97dbeeb8927afc127ceadb471509ee1cf0c5e59e
Luke Morton <lukemorton.designs@gmail.com>
2012-04-20 17:02:17 +0100

    MapperActions::find_one() arguments are all optional.

--- classes/mapperactions.php
+++ classes/mapperactions.php
@@ -4,7 +4,7 @@ interface MapperActions {
 	
 	public function find($suffix = NULL, array $where = NULL);
 
-	public function find_one($suffix, $id = NULL);
+	public function find_one($suffix = NULL, $where = NULL);
 
 	public function save(Model $model);
 
