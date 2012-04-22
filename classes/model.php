22ad290ddc7b0624d3a9c008c4441dd8541e5812
Luke Morton <lukemorton.designs@gmail.com>
2012-04-22 16:03:43 +0100

    Add some comments.

--- classes/model.php
+++ classes/model.php
@@ -1,9 +1,36 @@
 <?php
-
+/**
+ * A base class for domain logic
+ *
+ * The domain objects of your application should be souly
+ * focused on handling business logic. They shouldn't need to
+ * worry about how to persist the data they contain, nor
+ * should they be worried about whether the data has been
+ * persisted.
+ *
+ * This class provides one pseudo-magic method ::__object()
+ * for setting and getting the data transfer object.
+ *
+ *     $user = new Model_User;
+ *     $user->__object((object) array(
+ *         'name' => 'Luke',
+ *     ));
+ *     $object = $user->__object();
+ *
+ * @package     TheCure
+ * @category    Model
+ * @copyright   Gignite, 2011
+ */
 abstract class Model {
 
 	protected $object;
 	
+	/**
+	 * Get/set the data transfer object.
+	 *
+	 * @param   StdClass  If setting pass in a StdClass
+	 * @return  StdClass  If getting
+	 */
 	public function __object($object = NULL)
 	{
 		if ($object === NULL)
