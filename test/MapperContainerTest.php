7267c76e5455abeef9ee56a5af68f511eccd804d
Luke Morton <lukemorton.designs@gmail.com>
2012-04-27 16:50:17 +0100

    Add test for setting config via ::config().

--- test/MapperContainerTest.php
+++ test/MapperContainerTest.php
@@ -18,4 +18,12 @@ class MapperContainerTest extends PHPUnit_Framework_TestCase {
 			$container->mapper('User'));
 	}
 
+	public function testItShouldSetAndGetConfig()
+	{
+		$container = new MapperContainer('Array');
+		$expectedConfig = array();
+		$container->config($expectedConfig);
+		$this->assertSame($expectedConfig, $container->config());
+	}
+
 }
