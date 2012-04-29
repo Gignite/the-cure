8a9e0bef085b47ba276c6c7361f50d4c32630004
Luke Morton <lukemorton.designs@gmail.com>
2012-04-29 17:28:11 +0100

    Add Factory so that we can keep class factory logic in one place.

---
+++ classes/Gignite/TheCure/Factory.php
@@ -0,0 +1,73 @@
+<?php
+namespace Gignite\TheCure;
+
+use Gignite\TheCure\Mappers\Mapper;
+
+class Factory {
+
+	protected $config;
+	
+	public function __construct(array $config)
+	{
+		$this->config = $config;
+	}
+
+	protected function config()
+	{
+		return $this->config;
+	}
+
+	protected function prefix($type)
+	{
+		return $this->config['prefixes'][$type];
+	}
+
+	protected function separator()
+	{
+		return $this->config['separator'];
+	}
+
+	public function connection($suffix)
+	{
+		$class = $this->prefix('connection');
+		$class .= $this->separator();
+		$class .= $suffix;
+		return $class;
+	}
+
+	public function mapper($type, $suffix)
+	{
+		$class = $this->prefix('mapper');
+		$class .= $this->separator();
+		$class .= $type;
+		$class .= $this->separator();
+		$class .= $suffix;
+		return $class;
+	}
+
+	public function domain(Mapper $mapper)
+	{
+		$config = $this->config();
+		$class = get_class($mapper);
+		$domain = str_replace($this->prefix('mapper'), '', $class);
+		$domainPos = strrpos($domain, $this->separator()) + 1;
+		$domain = substr($domain, $domainPos);
+		return $domain;
+	}
+
+	public function model(Mapper $mapper, $suffix = NULL)
+	{
+		$class = $this->prefix('model');
+		$class .= $this->separator();
+		$class .= $this->domain($mapper);
+
+		if ($suffix !== NULL)
+		{
+			$class .= $this->separator();
+			$class .= $suffix;
+		}
+
+		return $class;
+	}
+
+}
