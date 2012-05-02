36f0fcf33026df262a8c426edfee01a5060cb6bd
Mathew Davies <thepixeldeveloper@googlemail.com>
2012-05-02 11:17:35 +0100

    Document the Mongo connection class

--- classes/Gignite/TheCure/Connections/Mongo.php
+++ classes/Gignite/TheCure/Connections/Mongo.php
@@ -10,13 +10,29 @@ namespace Gignite\TheCure\Connections;
 
 class Mongo implements Connection {
 
+	/**
+	 * @var \Mongo
+	 */
 	protected $connection;
 
+	/**
+	 * Setup a new MongoDB connection.
+	 *
+	 * @param array $config
+	 */
 	public function __construct(array $config)
 	{
 		$this->config = $config;
 	}
 
+	/**
+	 * Returns the config key if it exists otherwise
+	 * returns the default value.
+	 *
+	 * @param  string $key
+	 * @param  null   $default
+	 * @return string|null
+	 */
 	protected function config($key, $default = NULL)
 	{
 		if (isset($this->config[$key]))
@@ -26,12 +42,23 @@ class Mongo implements Connection {
 
 		return $default;
 	}
-	
+
+	/**
+	 * Creates a new MongoDB connection using the native PHP driver.
+	 *
+	 * @return \Mongo
+	 */
 	protected function connect()
 	{
 		return new \Mongo($this->config('connection', 'mongodb://127.0.0.1'));
 	}
 
+	/**
+	 * Connects to MongoDB if it hasn't already and selects the DB
+	 * specified in the configuration.
+	 *
+	 * @return \MongoDB
+	 */
 	public function get()
 	{
 		if ($this->connection === NULL)
