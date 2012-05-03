b19eee0009bbaa29ed82d424d635aa7e38392d26
Luke Morton <lukemorton.designs@gmail.com>
2012-05-03 16:38:33 +0100

    Add ContainerSetGet interface.

---
+++ classes/Gignite/TheCure/Mapper/ContainerSetGet.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * An interface for a mapper that uses a container
+ *
+ * @package     TheCure
+ * @category    Mapper
+ * @copyright   Gignite, 2012
+ */
+namespace Gignite\TheCure\Mapper;
+
+use Gignite\TheCure\Mapper\Container;
+
+interface ContainerSetGet {
+	
+	/**
+	 * @param   Container|null  $container
+	 * @return  mixed
+	 */
+	public function container(Container $container = NULL);
+
+}
