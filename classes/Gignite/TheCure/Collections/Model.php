077a8e3f157abd31b939b18dea853725cb92c6f9
Luke Morton <lukemorton.designs@gmail.com>
2012-05-03 14:21:20 +0100

    Remove untested logic.

--- classes/Gignite/TheCure/Collections/Model.php
+++ classes/Gignite/TheCure/Collections/Model.php
@@ -73,11 +73,6 @@ class Model extends Iterable {
 		}
 		else
 		{
-			if ( ! $object instanceOf Object)
-			{
-				$object = new Object($object);
-			}
-			
 			$model = new $class;
 			$model->__object($object);
 			$identities->set($model);
