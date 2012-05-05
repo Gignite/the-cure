793f209e2cd0aae2b3d08eaba0ac6736f4bf152c
Luke Morton <lukemorton.designs@gmail.com>
2012-05-05 23:26:40 +0100

    Add AttributeAliasTakenException.

---
+++ classes/Gignite/TheCure/AttributesAliasTakenException.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Attribute alias taken exception
+ *
+ * @package     TheCure
+ * @category    Attribute
+ * @category    Exception
+ * @copyright   Gignite, 2012
+ */
+namespace Gignite\TheCure;
+
+class AttributeAliasTakenException extends \Exception {}
